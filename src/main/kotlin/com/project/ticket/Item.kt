90aab7c5d9813a8345809dc6f1da72648afe0193
tcha <hataeck@gmail.com>
2025-12-23 23:59:19 +0900

    add test result for pessimistic lock and add optimistic lock

--- src/main/kotlin/com/project/ticket/Item.kt
+++ src/main/kotlin/com/project/ticket/Item.kt
@@ -2,11 +2,14 @@ package com.project.ticket
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Version
 
 @Entity
 class Item(
     @Id val id: Long, // 상품 ID (예: 1번)
-    var stockCount: Int // 재고 (예: 100개)
+    var stockCount: Int, // 재고 (예: 100개)
+    @Version
+    var version: Long = 0
 ) {
     protected constructor() : this(0L, 0)
 
